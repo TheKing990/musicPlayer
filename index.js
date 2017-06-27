98c09004fd0089ec05cdaea47fe34971182fbd25
Jhovany <jhovany990@gmail.com>
2017-06-27 14:57:00 -0500

    being able to stop or play on a click

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const ipc = require('electron').ipcRenderer;
 
  var sound = require('howler');
  var args;
+
+ var isPlaying =  false;
+
 ipc.on('model-music',function(event, arg) {
   console.log("hey fuck face" + arg);
   document.getElementById('helloP').innerHTML = arg;
@@ -15,8 +18,25 @@ ipc.on('model-music',function(event, arg) {
 })
 
 function PlayMe() {
+  document.getElementById('playbutton').innerHTML = 'Stop';
   sound = new Howl({
     src: [args]
   });
+
+
+// when you click stop and play the sound should stop or play
+// also the button text should change
+
+  if (isPlaying === false) {
+
     sound.play();
+    isPlaying = true;
+  }
+  else{
+
+    isPlaying = false;
+      document.getElementById('playbutton').innerHTML = 'Play';
+      sound.stop(args);
+  }
+
 }
