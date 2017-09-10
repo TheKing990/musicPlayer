f5c93b7f586c5f54bb76e1254633f6ee4acd978f
Jhovany <jhovany990@gmail.com>
2017-09-10 06:14:53 -0500

    able to display reddit post on ui just make sure the play button works

--- get_sub.js
+++ get_sub.js
@@ -8,10 +8,27 @@ var m = require('./config.js');
 request('https://www.reddit.com/r/music.json', function (error, response, body) {
   var obj = JSON.parse(body);
   console.log(obj);
-  console.log(obj.data.children[10].data.title);
-  console.log(obj.data.children[10].data.link_flair_text);
-  console.log(obj.data.children[10].data.permalink);
-  console.log(obj.data.children[10].data.author);
-  console.log(obj.data.children[10].data.url);
+  let list = '<ul class="list-group">';
+  var add;
+  for (var i = 0; i < 20; i++) {
+  //  var myspan = '<span style="visibility: hidden">' + i +'</span';
+      if (obj.data.children[i].data.link_flair_text == 'music streaming' || 'video') {
+        var lik = String(obj.data.children[i].data.url);
 
-});
+          $("#mylistgroup").append('<li class="list-group-item"><button type="button" class="btn btn-default navbar-btn" onclick = "PlayYouTube()"><span id="playbutton"class="glyphicon glyphicon-play" aria-hidden="true"></span></button>   ' + obj.data.children[i].data.title + '<span id=span_reddit style="visibility: hidden">' + i +'</span>' +'</li>');
+          console.log('<li class="list-group-item"><button type="button" class="btn btn-default navbar-btn" onclick = "PlayYouTube(' + obj.data.children[i].data.url + ')"><span id="playbutton"class="glyphicon glyphicon-play" aria-hidden="true"></span></button>   ' + obj.data.children[i].data.title + '</li>');
+        }
+
+      }
+})
+
+
+
+
+
+function PlayYouTube() {
+  $('ul li').click(function(){ console.log($(this).index());
+  });
+
+
+}
